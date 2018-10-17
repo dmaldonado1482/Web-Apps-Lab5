a1af1397fdbcf57f63f7391ac3c481ff007bf194
dmaldonado1482@gmail.com <dmaldonado1482@gmail.com>
2018-10-16 20:29:12 -0400

    Added styling.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -10,13 +10,22 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
         public string Rating { get; set; }
     }
 }
